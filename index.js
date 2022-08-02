149733b760cdddea2fced3dc7aaa23d240d92117
Barrett <barrettburnworth@gmail.com>
2022-08-02 15:18:25 -0500

    updated index.js for discord14

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const fs = require('node:fs');
 const path = require('node:path');
-const { Client, Collection, Intents } = require('discord.js');
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.commands = new Collection();
 
